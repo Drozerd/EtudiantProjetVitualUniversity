725c8bf894a73bbd093ad3a1a7bffabba51bd68e
Drozerd <debeneyf@gmail.com>
2022-12-27 10:14:29 +0100

    Y'a fallue refaire la route et le fichier XML

---
+++ src/main/java/org/mycompany/MainProjetV.java
@@ -0,0 +1,20 @@
+package org.mycompany;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.mycompany.route.CoursRoute;
+
+public class MainProjetV  {
+
+	public static void main(String[] args) throws Exception {
+		
+		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new CoursRoute());
+		context.start();
+		
+		Thread.sleep(2*60*1000);
+		context.stop();
+
+	}
+
+}
