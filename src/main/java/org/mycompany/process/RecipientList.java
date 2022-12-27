f1a64c25a4c42bdbcbc61331480b90d624f4fb77
Drozerd <debeneyf@gmail.com>
2022-12-27 12:38:09 +0100

    fdbwdb

--- src/main/java/org/mycompany/process/RecipientList.java
+++ src/main/java/org/mycompany/process/RecipientList.java
@@ -1,5 +1,12 @@
 package org.mycompany.process;
 
+import org.mycompany.model.Cours;
+
 public class RecipientList {
+	
+//	public String dispatchProf (Cours cours) 
+//	{
+//		if (cours.getEnseignant()=="")
+//	}
 
 }
